b2b6da70381e42c492fd9a654e521ae9ab3ec0c4
JasonSooter <7215306+functionalStoic@users.noreply.github.com>
2021-02-10 21:31:18 -0700

    fix(config): customize config to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,4 @@
 require(`dotenv`).config({
-<<<<<<< HEAD
-  path: `.env`,
-})
-
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-
-module.exports = {
-  siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-=======
   path: `.env`
 });
 
@@ -22,24 +12,10 @@ module.exports = {
     siteUrl: `https://www.boundplyobox.com/`,
     author: `Bound Plyobox`,
     siteDescription: `Highly-Rated, Strong Plyo Boxes built in Tulsa, Oklahoma`
->>>>>>> 61e858d (fix(config): add config)
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-<<<<<<< HEAD
-      // See the theme's README for all available options
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-=======
       options: {
         navigation: [
           {
@@ -54,21 +30,10 @@ module.exports = {
             title: `ABOUT`,
             slug: `/about`
           }
->>>>>>> 61e858d (fix(config): add config)
         ],
         externalLinks: [
           {
             name: `Twitter`,
-<<<<<<< HEAD
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
-          },
-        ],
-      },
-=======
             url: `https://twitter.com/boundplyobox`
           },
           {
@@ -77,32 +42,20 @@ module.exports = {
           }
         ]
       }
->>>>>>> 61e858d (fix(config): add config)
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-<<<<<<< HEAD
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-=======
         trackingId: process.env.GOOGLE_ANALYTICS_ID
       }
->>>>>>> 61e858d (fix(config): add config)
     },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-<<<<<<< HEAD
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-=======
         name: `Bound Plyobox - Strong Plyometric Boxes`,
         short_name: `Bound Plyobox`,
         description: `Highly-Rated Plyo Box built in Tulsa, Oklahoma`,
->>>>>>> 61e858d (fix(config): add config)
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
@@ -111,26 +64,15 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-<<<<<<< HEAD
-            type: `image/png`,
-=======
             type: `image/png`
->>>>>>> 61e858d (fix(config): add config)
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-<<<<<<< HEAD
-            type: `image/png`,
-          },
-        ],
-      },
-=======
             type: `image/png`
           }
         ]
       }
->>>>>>> 61e858d (fix(config): add config)
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
@@ -139,16 +81,8 @@ module.exports = {
       options: {
         analyzerMode: `static`,
         reportFilename: `_bundle.html`,
-<<<<<<< HEAD
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
-=======
         openAnalyzer: false
       }
     }
   ].filter(Boolean)
 };
->>>>>>> 61e858d (fix(config): add config)
